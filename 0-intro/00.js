b0b6d2cc8b0f9e4ec6c6af4d34c230ff8c659e67
Андрей Беляков <ttxiom@gmail.com>
2022-03-10 17:07:44 +0500

    update

--- 0-intro/00.js
+++
@@ -1,8 +0,0 @@
-const {print_to_rows, PI, get_rows, get_lines} = require("./module");
-
-let line = "Пермский государственный сельхоз универ";
-// print_to_rows(line);
-// console.log(get_rows(line));
-console.log(get_lines(line));
-
-console.log(`Pi = ${PI}`);
